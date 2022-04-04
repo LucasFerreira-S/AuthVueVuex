b1e00d8a2c75ff47ccbf387dbbdafa9ee7e909f0
Lucas <lucas.santos121252@gmail.com>
2022-04-04 15:43:41 -0300

    update: atualizando logout

--- src/views/Gerentes.vue
+++ src/views/Gerentes.vue
@@ -23,13 +23,7 @@ export default {
     this.$http.get('gerentes')
       .then(response => (this.gerentes = response.data))
       .catch(erro => console.log(erro));
-  },
-   beforeRouteEnter (to, from, next) {
-        if (!this.$store.state.token) {
-            next( { name: 'login' })
-        }
-        next()
-    }
+  }
 }
 </script>
 
