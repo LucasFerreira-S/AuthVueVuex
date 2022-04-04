c2e12e3bbb6b2ea8ad0e2a1b47d954f15fbe1f82
lucas <lucas.santos121252@gmail.com>
2022-04-04 10:08:36 -0300

    update: atualizando projeto para utilizar vuex

--- src/components/ BarraNavegacao.vue
+++ src/components/ BarraNavegacao.vue
@@ -20,7 +20,7 @@ export default {
   },
   computed: {
     usuarioestaLogado(){
-      return Boolean(localStorage.getItem('token'))
+      return Boolean(this.$store.state.token)
     }
   }
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -27,18 +27,8 @@ export default {
     },
     methods:{
         efetuarLogin(){
-            this.$http.post('auth/login', this.usuario)
-            .then(response => {
-                console.log(response)
-                // this.$store.state.token = response.data.access_token
-                 //this.$store.state.usuario = response.data.user
-                 this.$store.commit('DEFINIR_USUARIO_LOGADO',{
-                     token: response.data.access_token,
-                     usuario: response.data.user
-                 })
-                 this.$router.push({ name: 'gerentes' })
-            })
-            .catch(erro => console.log(erro))
+            this.$store.dispatch('efetuarLogin',this.usuario)
+            .then(()=>this.$router.push({name:'gerentes'}))
         }
     }
 }
