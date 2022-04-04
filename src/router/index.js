dfd4ca843da14a7c9abae07619c255d3c3a6bb6c
Lucas <lucas.santos121252@gmail.com>
2022-04-04 15:16:42 -0300

    update: Privando rotas

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from '../views/Home.vue'
 import NovoUsuario from '../views/NovoUsuario.vue'
 import Login from '../views/Login.vue'
 import http from '@/http'
-
+import provedor from '@/provedor'
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
@@ -25,17 +25,32 @@ const routes = [
   {
     path: '/cadastre-se',
     name: 'novo.usuario',
-    component: NovoUsuario
+    component: () => import('../views/NovoUsuario'),
+    meta: {
+      publica: true
+    }
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: Login,
+    meta: {
+      publica: true
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((routeTo, routeFrom, next) => {
+  if (!routeTo.meta.publica && !provedor.state.token) {
+    return next({
+      path:'/login'
+    });
+  }
+  next();
 })
 
 export default router
--- src/views/Gerentes.vue
+++ src/views/Gerentes.vue
@@ -23,7 +23,13 @@ export default {
     this.$http.get('gerentes')
       .then(response => (this.gerentes = response.data))
       .catch(erro => console.log(erro));
-  }
+  },
+   beforeRouteEnter (to, from, next) {
+        if (!this.$store.state.token) {
+            next( { name: 'login' })
+        }
+        next()
+    }
 }
 </script>
 
